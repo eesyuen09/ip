4aef206cfb97c35679bb43787dd68dd273321e50
Ee Syuen <eesyuen09@gmail.com>
2025-08-20 02:26:08 +0800

    Add echo

--- src/main/java/Penguin.java
+++ src/main/java/Penguin.java
@@ -1,12 +1,30 @@
+import java.util.Scanner;
+
 public class Penguin {
+    static String greetings = "Hello! I'm Penguin!\nWhat can I do for you?";
+    static String exit = "Bye. Hope to see you again soon!";
+    static String line = "____________________________________________________________________________";
+    static String input = "";
+    static Scanner sc = new Scanner(System.in);
+
+    public static void echo(String string) {
+        if (string.equals("bye")) {
+            System.out.println(line);
+            System.out.println(exit);
+            System.out.println(line);
+        } else {
+            System.out.println(line);
+            System.out.println(input);
+            System.out.println(line);
+            input = sc.nextLine();
+            echo(input);
+        }
+    }
+
     public static void main(String[] args) {
-        String greetings = "Hello! I'm Penguin.\nWhat can I do for you?";
-        String exit = "Bye. Hope to see you again soon!";
-        String line = "____________________________________________________________________________";
         System.out.println(line);
         System.out.println(greetings);
-        System.out.println(line);
-        System.out.println(exit);
-        System.out.println(line);
+        input = sc.nextLine();
+        echo(input);
     }
 }

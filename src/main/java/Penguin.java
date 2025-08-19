27fa7e3b0ba6e6c0df0bca1a03255aa95ada3fb9
Ee Syuen <eesyuen09@gmail.com>
2025-08-20 02:01:51 +0800

    Add greetings

---
+++ src/main/java/Penguin.java
@@ -0,0 +1,12 @@
+public class Penguin {
+    public static void main(String[] args) {
+        String greetings = "Hello! I'm Penguin.\nWhat can I do for you?";
+        String exit = "Bye. Hope to see you again soon!";
+        String line = "____________________________________________________________________________";
+        System.out.println(line);
+        System.out.println(greetings);
+        System.out.println(line);
+        System.out.println(exit);
+        System.out.println(line);
+    }
+}
